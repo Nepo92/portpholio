1ecf3b05bd4e167de7960bc4c1b8585fdc3b6dd3
Alexandr Eremeev <eremeev.a.eu@mail.ru>
2021-01-31 13:08:07 +0700

    changes fo safari

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,71 +1,64 @@
 window.addEventListener('scroll', () => {
-  const technologies = document.querySelectorAll('.technologies__column');
-  const techTitle = document.querySelector('.technologies__title');
+  if (innerWidth > 1550) {
+    const technologies = document.querySelectorAll('.technologies__column');
+    const techTitle = document.querySelector('.technologies__title');
 
-  if (window.pageYOffset >= 150) {
-    techTitle.style.opacity = '1';
-    techTitle.style.top = '0';
-  }
-
-  if (window.pageYOffset >= 180) {
-    for (let i = 0; i < technologies.length; i++) {
-      setTimeout(() => {
-        technologies[i].style.opacity = '1';
-        technologies[i].style.top = '0';
-      }, i * 300);
+    if (window.pageYOffset >= 150) {
+      techTitle.style.opacity = '1';
+      techTitle.style.top = '0';
     }
-  }
 
-  const works = document.querySelector('.works__inner');
+    if (window.pageYOffset >= 180) {
+      for (let i = 0; i < technologies.length; i++) {
+        setTimeout(() => {
+          technologies[i].style.opacity = '1';
+          technologies[i].style.top = '0';
+        }, i * 300);
+      }
+    }
 
-  if (window.pageYOffset >= 730) {
-    works.style.opacity = '1';
-    works.style.top = '0';
-  }
+    const works = document.querySelector('.works__inner');
 
-  const testimonialsTitle = document.querySelector('.testimonials__title');
-  const testimonials = document.querySelector('.testimonials__content');
+    if (window.pageYOffset >= 730) {
+      works.style.visibility = 'visible';
+      works.style.top = '0';
+    }
 
-  if (window.pageYOffset >= 1466) {
-    testimonialsTitle.style.opacity = '1';
-    testimonialsTitle.style.top = '0';
-    testimonials.style.opacity = '1';
-    testimonials.style.top = '0';
-  }
+    const testimonialsTitle = document.querySelector('.testimonials__title');
+    const testimonials = document.querySelector('.testimonials__content');
 
-  const orderTitle = document.querySelector('.order__title');
+    if (window.pageYOffset >= 1466) {
+      testimonialsTitle.style.visibility = 'visible';
+      testimonialsTitle.style.top = '0';
+      testimonials.style.visibility = 'visible';
+      testimonials.style.top = '0';
+    }
 
-  if (window.pageYOffset >= 1741) {
+    const orderTitle = document.querySelector('.order__title');
+
     orderTitle.style.opacity = '1';
     orderTitle.style.top = '0';
+
+    const orderInputs = document.querySelectorAll('input');
+
+    if (window.pageYOffset >= 1950) {
+      orderInputs.forEach((item, index) => {
+        if (index > 0) {
+          item.style.top = '0';
+          item.style.opacity = '1';
+        }
+      });
+
+      const orderTextarea = document.querySelector('.order__description');
+
+      orderTextarea.style.top = '0';
+      orderTextarea.style.opacity = '1';
+
+      const orderButton = document.querySelector('.order__button');
+
+      orderButton.style.opacity = '1';
+    }
   }
-
-  const orderInputs = document.querySelectorAll('input');
-  const orderTextarea = document.querySelector('.order__description');
-  const orderButton = document.querySelector('.order__button');
-
-  if (window.pageYOffset >= 1950) {
-    orderInputs.forEach((item, index) => {
-      if (index > 0) {
-        item.style.top = '0';
-        item.style.opacity = '1';
-      }
-    });
-
-    orderTextarea.style.top = '0';
-    orderTextarea.style.right = '0';
-    orderTextarea.style.opacity = '1';
-
-    orderButton.style.opacity = '1';
-  }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const headerIcon = document.querySelectorAll('.header__icon');
-
-  headerIcon.forEach((item) => {
-    item.style.opacity = '1';
-  });
 });
 
 /* Slider */
